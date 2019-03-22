b263e421012362b789515b3e7c18647b4b02e25b
Ilbis Candido <ilbis.candido@gmail.com>
2019-03-22 13:59:05 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TEST 1: seller findById ===");
@@ -47,6 +48,13 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Atualização concluida");
+
+		System.out.println("=== TEST 6: seller delete ===");
+		System.out.print("Digite o id para deletar: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Exclusão concluida ");
+		sc.close();
 	}
 
 }
